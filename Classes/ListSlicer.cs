df732b4e9647995987426053f372524f47ee9c3e
Kevin Stinson <postpoststruct@outlook.com>
2020-10-29 21:44:35 -0400

    Cleaned things up a little bit.

---
+++ Classes/ListSlicer.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ListSlicer
+{
+    public static class ListSlicer
+    {
+        public static int runCnt = 0;
+
+        public static void Print(TestParams tp)
+        {
+            Console.WriteLine(string.Format("{0}Test run #{1} [{2}]:",
+                (runCnt > 0 ? Environment.NewLine : string.Empty), ++runCnt, tp.PrintLists()));
+
+            var result = ListSlicer.Slice(tp.OriginalList, tp.RequestedLists);
+            int listCnt = 0;
+            foreach (List<int> l in result)
+            {
+                Console.WriteLine(string.Format("List# {0}:", ++listCnt));
+                foreach (int item in l)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+        }
+
+        public static List<List<int>> Slice(List<int> originalList, List<int> subListLengths)
+        {
+            List<List<int>> retVal = new List<List<int>>();
+            if (subListLengths == null || originalList == null)
+            {
+                throw new ArgumentException("Neither original list, nore subListsLengths can be null");
+            }
+
+            if (subListLengths.Count == 0)
+            {
+                retVal[0] = originalList;
+                return retVal;
+            }
+
+            int requestedItemCnt = GetRequestedItemCnt(subListLengths);
+            int originalListIndex = 0;
+            foreach (int subListLength in subListLengths)
+            {
+                List<int> subList = new List<int>();
+                for (int i = 0; i < subListLength; i++)
+                {
+                    if (originalListIndex < originalList.Count)
+                    {
+                        subList.Add(originalList[originalListIndex]);
+                    }
+                    originalListIndex++;
+                }
+
+                retVal.Add(subList);
+            }
+
+            return retVal;
+        }
+
+        private static int GetRequestedItemCnt(List<int> requestedItems)
+        {
+            int retVal = 0;
+            foreach (int item in requestedItems)
+            {
+                retVal += item;
+            }
+
+            return retVal;
+        }
+    }
+}
---
+++ Classes/TestParams.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ListSlicer
+{
+    public class TestParams
+    {
+        public List<int> OriginalList { get; set; }
+        public List<int> RequestedLists { get; set; }
+
+        public TestParams(List<int> originalList, List<int> requestedList)
+        {
+            OriginalList = originalList;
+            RequestedLists = requestedList;
+        }
+
+        public string PrintLists()
+        {
+            string retVal = string.Format("[originalList: {0}, sublist lengths: {1}]:",
+                ListToString(OriginalList),
+                ListToString(RequestedLists));
+
+            return retVal;
+        }
+
+        private string ListToString(List<int> requestedList)
+        {
+            string retVal = string.Empty;
+            if (requestedList.Count == 0)
+            {
+                return retVal;
+            }
+
+            for (int i = 0; i < requestedList.Count - 1; i++)
+            {
+                retVal += string.Format("{0},", requestedList[i]);
+            }
+
+            retVal += requestedList[requestedList.Count - 1];
+
+            return retVal;
+        }
+    }
+}
